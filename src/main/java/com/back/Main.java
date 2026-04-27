d582625859e7c93623f67e0ddecca682814d408c
bulmyung <bulmyun9@gmail.com>
2026-04-27 17:48:00 +0900

    4단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -7,36 +7,31 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
 
+
         Scanner scanner = new Scanner(System.in);
+
+        int lastId = 0;
+
 
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
-
-
             if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String content = scanner.nextLine().trim();
+                String wiseSayingContent = scanner.nextLine().trim();
                 System.out.print("작가 : ");
-                String author = scanner.nextLine().trim();
-                System.out.println("1번 명언이 등록되었습니다.");
+                String wiseSayingAuthor = scanner.nextLine().trim();
+
+                int no = ++lastId;
+
+                System.out.println("%d번 명언이 등록되었습니다".formatted(no));
             }
             if (cmd.equals("종료")) {
                 break;
-
-
-
-
-
-                }
-
-
-
             }
+        }
+            scanner.close();
 
-            
+        }
 
-        scanner.close();
     }
-}
-
